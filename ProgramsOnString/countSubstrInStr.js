d3a489446dd9bddb9acc6b1f9f39a1a693c182d5
Shraddha-Pathradkar <68277487+Shraddha-Pathradkar@users.noreply.github.com>
2021-11-09 16:53:40 -0800

    sorting and strings program

---
+++ ProgramsOnString/countSubstrInStr.js
@@ -0,0 +1,12 @@
+function naiveStringSearch(str, subStr) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < subStr.length; j++) {
+      if (str[i + j] !== subStr[j]) break;
+      if (j === subStr.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveStringSearch("heyomgiamshromgaddhaomgyes", "omg");
