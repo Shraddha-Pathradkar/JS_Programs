d14bd5091c1593ded701eb09b02f90a72936de80
Shraddha-Pathradkar <68277487+Shraddha-Pathradkar@users.noreply.github.com>
2021-11-10 09:50:20 -0800

    sorting bubble and selection sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,23 +1,32 @@
+//Time Complexity : O(n^2)
+
 const BubbleSort = (arr) => {
   for (let i = 0; i < arr.length - 2; i++) {
+    let change = 0;
+
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
+        change++;
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
+    if (change === 0) break;
   }
   return arr;
 };
 
 const BubbleSort = (arr) => {
   for (let i = 0; i < arr.length - 2; i++) {
+    let change = 0;
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
+        change++;
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
+    if (change === 0) break;
   }
   return arr;
 };
---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,15 @@
+const SelectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+};
+SelectionSort([23, 45, 23, 66, 7, 4, 399, 22]);
